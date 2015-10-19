123460652bbc7c98ddbf2777f43a8ed53a557f77
Josh Harbaugh <pixelhavenllc@gmail.com>
2015-10-19 11:21:08 -0400

    Pushed Meteor structured file/folders

---
+++ client/lib/app.js
@@ -0,0 +1,15 @@
+angular.module('socially',[
+  'angular-meteor',
+  'ui.router',
+  'angularUtils.directives.dirPagination',
+  'uiGmapgoogle-maps'
+]);
+
+function onReady() {
+  angular.bootstrap(document, ['socially']);
+}
+
+if (Meteor.isCordova)
+  angular.element(document).on("deviceready", onReady);
+else
+  angular.element(document).ready(onReady);
---
+++ server/startup/loadParties.js
@@ -0,0 +1,15 @@
+Meteor.startup(function () {
+  console.log('Server running...')
+  if (Parties.find().count() === 0) {
+    var parties = [
+      {'name': 'Dubstep-Free Zone',
+        'description': 'Fast just got faster with Nexus S.'},
+      {'name': 'All dubstep all the time',
+        'description': 'Get it on!'},
+      {'name': 'Savage lounging',
+        'description': 'Leisure suit required. And only fiercest manners.'}
+    ];
+    for (var i = 0; i < parties.length; i++)
+      Parties.insert({name: parties[i].name, description: parties[i].description});
+  }
+});
